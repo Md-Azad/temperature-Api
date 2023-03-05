78396c1782423694509283bbabd9c3c769b88b60
Md-Azad <sokalazad@gmail.com>
2023-03-05 16:04:21 +0100

    checking temperature by city name

---
+++ js/temp.js
@@ -0,0 +1,37 @@
+
+const api_key = `b6dcc8a74aa346dd01b3433aceab2b06`;
+const loadData = (city)=>{
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`)
+      .then(response => response.json())
+      .then(data => displayTemperature(data))
+}
+
+const displayTemperature = (data)=>{
+      console.log(data.weather[0].main)
+      const tempLoc = document.getElementById('temp');
+      tempLoc.innerText = data.main.temp; 
+      const cityCondition = document.getElementById('condition');
+      cityCondition.innerText = data.weather[0].main; 
+}
+
+      
+document.getElementById("btn-search").addEventListener('click',function(){
+
+      const cityName = document.getElementById('city-name');
+      console.log(cityName)
+
+      const takeData = document.getElementById('search-feild');
+      cityName.innerText= takeData.value;
+      const userInput = takeData.value;
+      
+      // console.log(cityCondition);
+      loadData(userInput)
+})
+
+loadData('dhaka')
+
+
+
+
+
+      
